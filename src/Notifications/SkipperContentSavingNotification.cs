b18f7978546674fc2a5ed3137ab995e635c43132
ZioTino <martinogabrielli@libero.it>
2021-11-17 17:04:15 +0100

    Resolved issue with invariant culture saving notification

--- src/Notifications/SkipperContentSavingNotification.cs
+++ src/Notifications/SkipperContentSavingNotification.cs
@@ -35,75 +35,54 @@ namespace Our.Umbraco.Skipper.Notifications
             // For each node that is being saved
             foreach (IContent node in notification.SavedEntities)
             {
-                bool nodeIsInvariant = true;
                 bool nameHasChanged = false;
                 List<ContentCultureInfos> changedCultureInfos = new List<ContentCultureInfos>();
 
                 // Node has invariant culture
                 if (node.CultureInfos.Count == 0)
                 {
-                    nodeIsInvariant = true;
                     nameHasChanged = node.IsPropertyDirty("Name");
-
-                    // With this I avoid some code duplcates
-                    changedCultureInfos.Add(new ContentCultureInfos(null));
                 }
                 else
                 {
-                    foreach (var culture in node.CultureInfos)
+                    foreach (var c in node.CultureInfos)
                     {
-                        changedCultureInfos.Add(culture);
+                        changedCultureInfos.Add(c);
                     }
 
-                    nodeIsInvariant = false;
                     nameHasChanged = changedCultureInfos.Count() > 0;
                 }
 
                 // Name or node hasn't changed, so skip the current iteration
                 if (node.HasIdentity && !nameHasChanged) { continue; }
 
-                IPublishedContent baseNode;
-                foreach (ContentCultureInfos cultureInfos in changedCultureInfos)
+                // Node has invariant culture
+                if (node.CultureInfos.Count == 0)
                 {
-                    string culture = cultureInfos.Culture ?? null;
+                    IPublishedContent baseNode;
                     try
                     {
-                        // If node has no parent, we can skip the current iteration
-                        // This is only for root nodes
-                        if (node.HasIdentity && node.Level == 0 && node.ParentId == 0) { continue; }
-
-                        baseNode = umbracoContext.Content.GetById(node.ParentId);
-
-                        // Some best practice to avoid infinite loops
-                        int count = 0;
-                        // If baseNode is skipper's work, we need to find the first eligible node to be our rootNode
-                        while (baseNode != null && baseNode.Parent != null && baseNode.Parent.Id != 0 && baseNode.SkipperWasHere(culture))
-                        {
-                            baseNode.Parent.SkipperWasHere(out baseNode, culture, true);
-
-                            count++;
-                            if (count >= SkipperConfiguration.WhileLoopMaxCount) { break; }
-                        }
+                        baseNode = FindRootNode(umbracoContext, node, null);
                     }
                     catch { continue; }
 
                     int duplicateNodes = 0;
                     int maxNumber = 0;
 
-                    foreach (IPublishedContent sibling in baseNode.SiblingsAndSelf(culture))
+                    foreach (IPublishedContent sibling in baseNode.SiblingsAndSelf())
                     {
                         // If for some reasons the baseNode is still Skipper's work we need to check for duplicates anyway
-                        if (baseNode.SkipperWasHere(culture))
+                        if (baseNode.SkipperWasHere(culture: null))
                         {
-                            CheckPublishedContentName(node, sibling, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, cultureInfos);
+                            CheckPublishedContentName(node, sibling, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, null);
                         }
 
                         // We need to check if some of it's children's name is the same as some of it's siblings
-                        foreach (IPublishedContent children in sibling.Children(culture))
+                        foreach (IPublishedContent children in sibling.Children())
                         {
-                            if (children.SkipperWasHere(culture))
+                            if (children.SkipperWasHere(culture: null))
                             {
-                                CheckPublishedContentName(node, children, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, cultureInfos);
+                                CheckPublishedContentName(node, children, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, null);
                             }
                         }
                     }
@@ -115,19 +94,85 @@ namespace Our.Umbraco.Skipper.Notifications
                         // The last (1) is added by angular.js, maybe because Umbraco thinks it's a duplicate name
                         // This is a minor issue IMHO, because the content editor is supposed to modify the name to make it unique anyway afterwards
                         string sectionToAdd = " (" + (maxNumber + 1).ToString() + ") ";
-                        if (nodeIsInvariant)
+                        node.Name += sectionToAdd;
+                    }
+                }
+                else
+                {
+                    foreach (ContentCultureInfos cultureInfos in node.CultureInfos)
+                    {
+                        IPublishedContent baseNode;
+                        string culture = null;
+                        if (cultureInfos is not null) // Just in case
                         {
-                            // Node is invariant, we can set name
-                            node.Name += sectionToAdd;
+                            culture = cultureInfos.Culture;
                         }
-                        else
+
+                        try
                         {
+                            baseNode = FindRootNode(umbracoContext, node, culture);
+                        }
+                        catch { continue; }
+
+                        int duplicateNodes = 0;
+                        int maxNumber = 0;
+
+                        foreach (IPublishedContent sibling in baseNode.SiblingsAndSelf(culture))
+                        {
+                            // If for some reasons the baseNode is still Skipper's work we need to check for duplicates anyway
+                            if (baseNode.SkipperWasHere(culture))
+                            {
+                                CheckPublishedContentName(node, sibling, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, cultureInfos);
+                            }
+
+                            // We need to check if some of it's children's name is the same as some of it's siblings
+                            foreach (IPublishedContent children in sibling.Children(culture))
+                            {
+                                if (children.SkipperWasHere(culture))
+                                {
+                                    CheckPublishedContentName(node, children, duplicateNodes, maxNumber, out duplicateNodes, out maxNumber, cultureInfos);
+                                }
+                            }
+                        }
+
+                        if (duplicateNodes > 0)
+                        {
+                            // The final space at the end is to make sure that Umbraco doesn't think that this is a duplicate node
+                            // If we remove it, the name will be {node.Name} + ({maxNumber + 1}) + (1)
+                            // The last (1) is added by angular.js, maybe because Umbraco thinks it's a duplicate name
+                            // This is a minor issue IMHO, because the content editor is supposed to modify the name to make it unique anyway afterwards
+                            string sectionToAdd = " (" + (maxNumber + 1).ToString() + ") ";
+
                             // We change the culture Name for each culture that has changed
                             node.SetCultureName(node.GetCultureName(culture) + sectionToAdd, culture);
                         }
                     }
                 }
+
             }
+        }
+
+        private static IPublishedContent FindRootNode(IUmbracoContext umbracoContext, IContent node, string culture = null)
+        {
+            IPublishedContent baseNode;
+            // If node has no parent, we can skip the current iteration
+            // This is only for root nodes
+            if (node.HasIdentity && node.Level == 0 && node.ParentId == 0) { return null; }
+
+            baseNode = umbracoContext.Content.GetById(node.ParentId);
+
+            // Some best practice to avoid infinite loops
+            int count = 0;
+            // If baseNode is skipper's work, we need to find the first eligible node to be our rootNode
+            while (baseNode != null && baseNode.Parent != null && baseNode.Parent.Id != 0 && baseNode.SkipperWasHere(culture))
+            {
+                baseNode.Parent.SkipperWasHere(out baseNode, culture, true);
+
+                count++;
+                if (count >= SkipperConfiguration.WhileLoopMaxCount) { break; }
+            }
+
+            return baseNode;
         }
 
         private static void CheckPublishedContentName(IContent node, IPublishedContent content, int duplicateNodes, int maxNumber, out int _duplicateNodes, out int _maxNumber, ContentCultureInfos cultureInfos = null)
@@ -142,7 +187,11 @@ namespace Our.Umbraco.Skipper.Notifications
                 string childrenName = content.Name.ToLower().TrimEnd();
                 string nodeName;
 
-                string culture = cultureInfos.Culture ?? null;
+                string culture = null;
+                if (cultureInfos is not null)
+                {
+                    culture = cultureInfos.Culture;
+                }
 
                 // If node is NOT invariant
                 if (!string.IsNullOrEmpty(culture))
